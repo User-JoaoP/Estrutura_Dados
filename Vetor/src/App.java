790b2d9b5cb0970a5ea1cac00c90c077a4cd3f93
User-JoaoP <joaopedrovs3003@gmail.com>
2022-06-21 08:09:23 -0300

    Adição da questão F no ex. de Array

--- Vetor/src/App.java
+++ Vetor/src/App.java
@@ -9,13 +9,17 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int [] numeros = {3,4,5,43,6,4,2,7,8,888};
+        int [] numeros = {3,9,5,43,6,4,2,7,8,3};
         int maior = 0;
         int menor = numeros[0];
         double media = 0;
+        boolean repetidos = false;
         
+        System.out.print("Vetor: ");
+
         for (int i = 0; i < numeros.length; i++){
-            System.out.println(numeros[i]);
+            System.out.print(numeros[i]);
+            System.out.print(" ");
         
             if(maior < numeros[i]){
                 maior = numeros[i];
@@ -24,6 +28,12 @@ public class App {
             if(menor > numeros[i]){
                 menor = numeros[i];
             }
+
+            for (int j = i + 1; j < numeros.length; j++){
+                if (numeros[i] == numeros[j])
+                    repetidos = true;
+            }
+
          }
 
          for (int i = 0; i < numeros.length; i++){
@@ -31,7 +41,9 @@ public class App {
          }
          media = media / numeros.length;
 
-         System.out.printf("O maior é: %d",maior);
+         if (repetidos == true)
+            System.out.print("\n\nExistem repetidos.");
+         System.out.printf("\nO maior é: %d",maior);
          System.out.printf("\nO menor é: %d",menor);
          System.out.printf("\nA média de todos os números no vetor é: %.2f",media);
     }
